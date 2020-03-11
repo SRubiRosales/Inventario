9771d797eaca9dd82c49b75aea3c3cb0e37626f5
SRubiRosales <60980248+SRubiRosales@users.noreply.github.com>
2020-03-11 12:06:50 -0600

    Initial commit

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Usuarios | Inventario</title>
+</head>
+<body>
+    <div class="container" align="center">
+        <h1>Usuarios</h1>
+        <img src="{{ asset('images/usuarios.jpeg') }}" alt="Imagen de usuarios">
+    </div>
+</body>
+</html>
